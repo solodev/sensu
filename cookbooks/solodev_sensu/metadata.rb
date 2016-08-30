adf55f7d8bd6b7169562919f3b01889942faa273
Sean Porter <portertech@gmail.com>
2016-08-30 15:43:40 -0700

    test-kitchen init, begin work on sensu wrapper

--- cookbooks/solodev_sensu/metadata.rb
+++ cookbooks/solodev_sensu/metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures solodev_sensu'
 long_description 'Installs/Configures solodev_sensu'
 version '0.1.0'
 
+depends "sensu"
+
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
 # issues_url 'https://github.com/<insert_org_here>/solodev_sensu/issues' if respond_to?(:issues_url)
--- site-cookbooks/solodev_sensu/recipes/default.rb
+++ site-cookbooks/solodev_sensu/recipes/default.rb
@@ -3,3 +3,5 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 Solodev, All Rights Reserved.
+
+include_recipe "sensu"
