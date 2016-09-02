f9c5ef3abafbc15764be00f3961590d963de3f44
Sean Porter <portertech@gmail.com>
2016-09-02 15:35:58 -0700

    add sensu clients to rabbitmq cluster

--- sparkleformation/sensu.rb
+++ sparkleformation/sensu.rb
@@ -256,7 +256,7 @@ SparkleFormation.new(:sensu).load(:base).overrides do
         custom_security_group_ids [ref!(:sensu_security_group), ref!(:rabbitmq_security_group)]
         custom_recipes do
           setup [ "solodev_sensu::rabbitmq" ]
-          configure [ "solodev_sensu::rabbitmq_#{type}" ]
+          configure [ "solodev_sensu::rabbitmq_#{type}", "solodev_sensu::client" ]
           deploy []
           undeploy []
           shutdown []
