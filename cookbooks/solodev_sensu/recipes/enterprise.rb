fd6b61198af240fe0a8be8c67e0e4c0d76626bfc
Sean Porter <portertech@gmail.com>
2016-09-02 13:45:51 -0700

    working sensu enterprise dashboard

--- cookbooks/solodev_sensu/recipes/enterprise.rb
+++ cookbooks/solodev_sensu/recipes/enterprise.rb
@@ -13,3 +13,21 @@ node.override["sensu"]["redis"]["port"] = node["Elasticache"]["Port"].to_i
 
 include_recipe "sensu::enterprise"
 include_recipe "sensu::enterprise_service"
+
+credentials = get_sensu_state(node, "enterprise", "repository", "credentials")
+repository_url = "#{node["sensu"]["enterprise"]["repo_protocol"]}://#{credentials['user']}:#{credentials['password']}@#{node["sensu"]["enterprise"]["repo_host"]}"
+
+yum_repository "sensu-enterprise-dashboard" do
+  description "sensu enterprise dashboard"
+  url "#{repository_url}/yum/x86_64/"
+  action :add
+  gpgcheck false
+end
+
+package "sensu-enterprise-dashboard" do
+  version node["sensu"]["enterprise-dashboard"]["version"]
+end
+
+sensu_dashboard_config node.name
+
+include_recipe "sensu::enterprise_dashboard_service"
