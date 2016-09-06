738a200776f35c6e56ea963b5cb8f3ab79092248
Sean Porter <portertech@gmail.com>
2016-09-06 13:33:07 -0700

    use citadel for customer clients (for now)

--- site-cookbooks/solodev_sensu/recipes/customer_client.rb
+++ site-cookbooks/solodev_sensu/recipes/customer_client.rb
@@ -10,7 +10,10 @@
 node.override["sensu"]["rabbitmq"]["hosts"] = []
 
 # TODO: Figure out secrets management for customer OpsWorks stacks,
-# for the Sensu client SSL certificate/key and credentials.
+# for the Sensu client SSL certificate/key. Currently using citadel
+# with the Sensu OpsWorks stack S3 bucket.
+set_sensu_state(node, "ssl", "client", "cert", citadel["sensu/ssl/client/cert.pem"])
+set_sensu_state(node, "ssl", "client", "key", citadel["sensu/ssl/client/key.pem"])
 
 include_recipe "sensu"
 
